73d8f668202cbbfc5815616c6c0ec0149e182b62
Avikant <apple@Avikants-iMac.local>
2014-07-17 17:40:52 +0530

    Initial Commit

---
+++ 5x5/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  5x5
+//
+//  Created by Avikant on 07/17/14.
+//  Copyright (c) 2014 Avikant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ 5x5Tests/_x5Tests.m
@@ -0,0 +1,34 @@
+//
+//  _x5Tests.m
+//  5x5Tests
+//
+//  Created by Avikant on 07/17/14.
+//  Copyright (c) 2014 Avikant. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _x5Tests : XCTestCase
+
+@end
+
+@implementation _x5Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
